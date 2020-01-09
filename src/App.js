058770802d6a4737e1a6adbed0598ebee91934fa
xatkx <xatkx1@gmail.com>
2020-01-09 19:10:12 -0400

    klk

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         soy  el mejor
+        </p>
+        <a
+          className="App-link"
+          href="https://gibhub.com/xatkx"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
